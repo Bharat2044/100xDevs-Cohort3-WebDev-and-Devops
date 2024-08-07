bb8b3bfb49d54bcf0da72160dc3f4c3fe80e85c5
Bharat Kumar <bk9955204456@gmail.com>
2024-08-07 10:33:52 +0530

    Portfolio Returns Calculator project javascript code added

--- Cohort 2 Warmup Videos/Assignment Solution/Portfolio-Returns-Calculator/script.js
+++ Cohort 2 Warmup Videos/Assignment Solution/Portfolio-Returns-Calculator/script.js
@@ -1,8 +1,10 @@
 function calculateReturns() {
+  // Get the values from the input fields
   const capital = document.getElementById("capital").value;
   const years = document.getElementById("years").value;
   const interest = document.getElementById("interest").value;
 
+  // Check if the input fields are not empty and calculate the total amount 
   if (capital !== "" && years !== "" && interest !== "") {
     const totalAmount = capital * Math.pow(1 + interest / 100, years);
 
