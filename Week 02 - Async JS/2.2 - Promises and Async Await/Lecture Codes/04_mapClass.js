07dd9c932701749ad0ebb685ede51044302a62b7
Bharat Kumar <bk9955204456@gmail.com>
2024-08-11 20:52:08 +0530

    2.2 Promises and Async Await lecture/class code added

---
+++ Week 02 - Async JS/2.2 - Promises and Async Await/Lecture Codes/04_mapClass.js
@@ -0,0 +1,13 @@
+// create a map object with key-value pairs
+const map = new Map();
+
+// Set key-value pairs in the map object
+map.set('name', 'Bharat');
+map.set('age', 21);
+
+// Log the map object
+console.log(map); // Output: Map(2) { 'name' => 'Bharat', 'age' => 21 }
+
+// Get the value of the key 'name' and 'age' from the map object
+console.log(map.get('name')); // Output: Bharat
+console.log(map.get('age')); // Output: 21
