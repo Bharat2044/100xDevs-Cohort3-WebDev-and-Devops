c00d27255448dde07aea46637b74244663fdbf91
Bharat Kumar <bk9955204456@gmail.com>
2024-08-17 22:04:05 +0530

    3.1 - DOM Manipulation lecture/class code added

---
+++ Week 03 - DOM Manipulation/3.1 - DOM Manipulation/Lecture Codes/2. Updating Elements/index.js
@@ -0,0 +1,13 @@
+document.querySelectorAll("h4")[1].innerHTML = "Hello World";
+
+
+let counter = 0;
+
+function callback() {
+  const element = document.querySelector("h2")[1];
+  element.innerHTML = counter;
+  // console.log(counter);
+  counter++;  
+}
+
+setInterval(callback, 1000);
