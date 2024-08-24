2a42328537b68a5ca6c837b8e39a1b15692058b3
Bharat Kumar <bk9955204456@gmail.com>
2024-08-24 21:05:11 +0530

    4.1 - NodeJS and the JS Runtime class/lecture code added

---
+++ Week 04 - NodeJS and the JS Runtime/4.1 - NodeJS and the JS Runtime/Lecture Codes/01_Node and Bun/index.js
@@ -0,0 +1,17 @@
+/**
+ * Use `node fileName.js` command to run this file
+ * Can also use `bun fileName.js` to run this file
+ */
+
+// function to add two numbers and return the sum
+function sum (a, b) {
+    return a + b;
+}
+
+// calling the function and storing the result in a variable ans
+const ans = sum(1, 2);
+
+// printing the result to the console
+console.log(ans);
+
+
