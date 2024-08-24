8fafa0bd899e0ce77a278550d2ce1b4dafe0a27e
Bharat Kumar <bk9955204456@gmail.com>
2024-08-24 21:04:34 +0530

    4.1 - NodeJS and the JS Runtime class/lecture code added

---
+++ Week 04 - NodeJS and the JS Runtime/4.1 - NodeJS and the JS Runtime/Lecture Codes/03_npm/index.js
@@ -0,0 +1,7 @@
+// import chalk module
+import chalk from 'chalk';
+
+console.log(chalk);
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.green.underline('This is a success message'));
