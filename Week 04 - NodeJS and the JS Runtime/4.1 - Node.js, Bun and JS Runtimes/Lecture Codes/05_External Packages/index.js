13e38fd493d0577ab070fb5ee113ea675077b53e
Bharat Kumar <bk9955204456@gmail.com>
2024-08-25 16:57:27 +0530

    4.1 - Node.js, Bun and JS Runtimes class/lecture code added

---
+++ Week 04 - NodeJS and the JS Runtime/4.1 - Node.js, Bun and JS Runtimes/Lecture Codes/05_External Packages/index.js
@@ -0,0 +1,13 @@
+// import chalk module
+import chalk from 'chalk';
+
+console.log(chalk);
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.green.underline('This is a success message'));
+
+
+// import express module
+import express from 'express';
+
+console.log(express);
