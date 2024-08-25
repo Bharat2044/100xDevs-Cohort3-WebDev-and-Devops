0640d076e18d896c5045c324181b0bad31b3b8bf
Bharat Kumar <bk9955204456@gmail.com>
2024-08-25 16:58:21 +0530

    4.1 - Node.js, Bun and JS Runtimes class/lecture code added

---
+++ Week 04 - NodeJS and the JS Runtime/4.1 - Node.js, Bun and JS Runtimes/Lecture Codes/02_Starting NodeJS Project/index.js
@@ -0,0 +1,18 @@
+/**
+ * You can run this code by using the following command
+ *   $ node index.js
+ *   $ bun index.js
+ *   $ npm run start
+ *   $ npm run bharat
+ */
+
+// function to add two numbers and return the sum
+function sum (a, b) {
+    return a + b;
+}
+
+// calling the sum function and storing the result in a variable ans
+const ans = sum(1, 2);
+
+// printing the result to the console
+console.log(ans);
