2b8cb9a9cf51a52fafc236aebef685c4625de09d
Bharat Kumar <bk9955204456@gmail.com>
2024-08-31 21:21:54 +0530

    5.1 - HTTP Headers, Middlewares lecture code added

---
+++ Week 05 - HTTP - Headers, Middlewares, File Storage/5.1 - HTTP Headers, Middlewares/Lecture Codes/02_Fetch API using Axios External Library/script.js
@@ -0,0 +1,26 @@
+// funtion to fetch data from the server
+async function fetchPost() {
+    // log the message to the console before sending the request
+    console.log("Before Sending Request");    
+
+    // fetch data from the server using the fetch method and store it in the response variable
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+
+    // convert the response to json format and store it in the data variable
+    const data = response.data;
+
+    // log the data to the console
+    console.log(data);
+
+    // log the message to the console after receiving the response
+    console.log("After Receiving Response");
+
+    // display the title of the post in the browser
+    // document.getElementById("posts").innerText = data.title;
+
+    // display the data in the browser as a string
+    document.getElementById("posts").innerText = JSON.stringify(data);
+}
+
+// call the fetchPost function 
+fetchPost();
