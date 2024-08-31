96e76d3cfb08b710f7a4a77f29abe644be8e0d1c
Bharat Kumar <bk9955204456@gmail.com>
2024-08-31 21:25:24 +0530

    5.1 - HTTP Headers, Middlewares lecture code added

--- Week 05 - HTTP - Headers, Middlewares, File Storage/5.1 - HTTP Headers, Middlewares/Lecture Codes/03_Create an HTTP Server/index.js
+++ Week 05 - HTTP - Headers, Middlewares, File Storage/5.1 - HTTP Headers, Middlewares/Lecture Codes/03_Create an HTTP Server/index.js
@@ -18,51 +18,51 @@ const express = require("express");
 // create an express application using express function
 const app = express();
 
-// create a get route with the path /sum 
-app.get("/sum", function(req, res) {
+// create a get route with the path /sum
+app.get("/sum", function (req, res) {
     // get the values of a and b from the query parameters and convert them to integers
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
 
     // send the sum of a and b as a response in json format
     res.json({
-        ans: a + b
+        ans: a + b,
     });
 });
 
 // create a get route with the path /subtract
-app.get("/subtract", function(req, res) {
+app.get("/subtract", function (req, res) {
     // get the values of a and b from the query parameters and convert them to integers
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
 
     // send the difference of a and b as a response in json format
     res.json({
-        ans: a - b
+        ans: a - b,
     });
 });
 
 // create a get route with the path /multiply
-app.get("/multiply", function(req, res) {
+app.get("/multiply", function (req, res) {
     // get the values of a and b from the query parameters and convert them to integers
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
 
     // send the product of a and b as a response in json format
     res.json({
-        ans: a * b
+        ans: a * b,
     });
 });
 
 // create a get route with the path /divide
-app.get("/divide", function(req, res) {
+app.get("/divide", function (req, res) {
     // get the values of a and b from the query parameters and convert them to integers
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
 
     // send the division of a and b as a response in json format
     res.json({
-        ans: a / b
+        ans: a / b,
     });
 });
 
