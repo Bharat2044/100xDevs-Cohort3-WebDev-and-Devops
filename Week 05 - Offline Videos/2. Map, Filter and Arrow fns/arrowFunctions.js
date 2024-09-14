89738a807469a19f8fac449de0174d569597e0ea
Bharat Kumar <bk9955204456@gmail.com>
2024-09-14 17:11:25 +0530

    arrow functions code added

---
+++ Week 05 - Offline Videos/2. Map, Filter and Arrow fns/arrowFunctions.js
@@ -0,0 +1,77 @@
+/*
+// Normal function to print Hello World
+function greet() {
+    console.log('Hello World!');
+}
+
+greet(); // Hello World!
+*/
+
+/*
+// Arrow function to print Hello World
+const greeet = () => {
+    console.log('Hello World!');
+}
+
+greet(); // Hello World!
+*/
+
+// Arrow function to print Hello World - Shorter version
+// const greet = () => console.log('Hello World!');
+
+greet(); // Hello World!
+
+
+/*
+// Normal function to greet a person
+function greetPerson(name) {
+    console.log(`Hello ${name}!`);
+}
+
+greetPerson('Bharat'); // Hello Bharat!
+*/
+
+/*
+// Arrow functions to greet a person
+const greetPerson = (name) => {
+    console.log(`Hello ${name}!`);
+};
+
+greetPerson('Bharat'); // Hello Bharat!
+*/
+
+// Arrow functions to greet a person - Shorter version
+const greetPerson = (name) => console.log(`Hello ${name}!`);
+
+greetPerson('Bharat'); // Hello Bharat!
+
+
+/*
+// Normal function to add two numbers
+function sum(a, b) {
+    // return sum of a and b
+    return a + b;
+}
+
+const add = sum(5, 6);
+console.log(add); // 11
+*/
+
+/*
+// Arrow functions to add two numbers
+const sum = (a, b) => {
+
+    // return sum of a and b
+    return a + b;
+};
+
+const add = sum(5, 6);
+console.log(add); // 11
+*/
+
+// Arrow functions to add two numbers - Shorter version
+const sum = (a, b) => a + b;
+
+const add = sum(5, 6);
+console.log(add); // 11
+
