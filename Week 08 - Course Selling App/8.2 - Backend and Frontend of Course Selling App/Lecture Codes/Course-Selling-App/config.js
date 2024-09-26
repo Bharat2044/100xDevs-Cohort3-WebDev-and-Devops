58d692827009369f14b8fa663f49dea574e1194e
Bharat Kumar <bk9955204456@gmail.com>
2024-09-26 09:13:11 +0530

    Added .env file - don't push this file ion github

--- Week 08 - Course Selling App/8.2 - Backend and Frontend of Course Selling App/Lecture Codes/Course-Selling-App/config.js
+++
@@ -1,7 +0,0 @@
-const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
-
-module.exports = {
-    JWT_ADMIN_PASSWORD,
-    JWT_USER_PASSWORD,
-};
