1e5293d75da1bf04d245d07bdde4568c1e4f1c02
Bharat Kumar <bk9955204456@gmail.com>
2024-10-12 18:01:52 +0530

    Assignment 2 Solution - notifications-count solution code added

--- Week 09 - Offline Videos/Assignment Solution/Assignment 2 Solution - notifications-count/src/App.jsx
+++ Week 09 - Offline Videos/Assignment Solution/Assignment 2 Solution - notifications-count/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import the Notifications component
+import Notifications from "./Notifications";
 
+// Create a function component named App that will be rendered in the root element
 function App() {
-  const [count, setCount] = useState(0)
+  
+    // return JSX that will be rendered
+    return (
+        // Create a div element
+        <div>
+            {/* Add a heading */}
+            <h1>Linkedin Notifications Count</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            {/* Render the Notifications component */}
+            <Notifications />
+        </div>
+    );
 }
 
-export default App
+// Export the App component to use it in the other files
+export default App;
