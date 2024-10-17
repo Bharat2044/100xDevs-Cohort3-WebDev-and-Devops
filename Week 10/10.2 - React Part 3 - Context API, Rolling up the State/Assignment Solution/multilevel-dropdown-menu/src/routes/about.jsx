d0dae945f29c7a2548ac6abe8c2d616c579f095c
Bharat Kumar <bk9955204456@gmail.com>
2024-10-17 19:30:31 +0530

    10.2 - React Part 3 - Context API, Rolling up the State assignment solution code added

---
+++ Week 10/10.2 - React Part 3 - Context API, Rolling up the State/Assignment Solution/multilevel-dropdown-menu/src/routes/about.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <h1>About page content</h1>;
+};
+
+export default About;
---
+++ Week 10/10.2 - React Part 3 - Context API, Rolling up the State/Assignment Solution/multilevel-dropdown-menu/src/routes/frontend.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Frontend = () => {
+  return <h1>Frontend page content</h1>;
+};
+
+export default Frontend;
