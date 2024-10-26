f967263853f14a86c6c9531c494928012be8e4a9
Bharat Kumar <bk9955204456@gmail.com>
2024-10-26 17:47:53 +0530

    10.1 - React Part 2 (SPAs, Routing) assignment solution code added

---
+++ Week 10/10.1 - React Part 2 (SPAs, Routing)/Assignment Solution/allen-clone/src/menuItems.jsx
@@ -0,0 +1,158 @@
+export const menuItems = [
+    {
+        title: "Exams",
+        submenu: [
+            {
+                title: "Exams",
+                submenu: [
+                    {
+                        title: "Neet",
+                        url: "neet",
+                        submenu: [
+                            {
+                                title: "Class 11th",
+                                url: "online-coaching-class-11",
+                            },
+                            {
+                                title: "Class 12th",
+                                url: "online-coaching-class-12",
+                            },
+                            {
+                                title: "Class 12th Plus",
+                                url: "online-coaching-class-12-droppers",
+                            },
+                        ],
+                    },
+                    {
+                        title: "JEE",
+                        url: "jee",
+                        submenu: [
+                            {
+                                title: "Class 11th",
+                                url: "online-coaching-class-11",
+                            },
+                            {
+                                title: "Class 12th",
+                                url: "online-coaching-class-12",
+                            },
+                            {
+                                title: "Class 12th Plus",
+                                url: "online-coaching-class-12-droppers",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Class 6-10",
+                        url: "classes-6-10",
+                        submenu: [
+                            {
+                                title: "Class 6th",
+                                url: "class-6",
+                            },
+                            {
+                                title: "Class 7th",
+                                url: "class-7",
+                            },
+                            {
+                                title: "Class 8th",
+                                url: "class-8",
+                            },
+                            {
+                                title: "Class 9th",
+                                url: "class-9",
+                            },
+                            {
+                                title: "Class 10th",
+                                url: "class-10",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Programs",
+        submenu: [
+            {
+                title: "Online Programs",
+                url: "ultimate-program-live-courses",
+            },
+            {
+                title: "Class Programs",
+                link: "https://www.allen.ac.in/",
+            },
+            {
+                title: "Distance Learning",
+                link: "https://dlp.allen.ac.in/",
+            }
+        ]
+    },
+    {
+        title: "Scholarship",
+        submenu: [
+            {
+                title: "ADSAT",
+                url: "adsat-register",
+            },
+            {
+                title: "TALLENTEX",
+                link: "https://studyonline.tallentex.com/",
+            }
+        ]
+    },
+    {
+        title: "Test Series",
+        submenu: [
+            {
+                title: "Exams",
+                submenu: [
+                    {
+                        title: "Neet",
+                        url: "neet",
+                        submenu: [
+                            {
+                                title: "Class 11th",
+                                url: "online-coaching-class-11",
+                            },
+                            {
+                                title: "Class 12th",
+                                url: "online-coaching-class-12",
+                            },
+                            {
+                                title: "Class 12th Plus",
+                                url: "online-coaching-class-12-droppers",
+                            },
+                        ],
+                    },
+                    {
+                        title: "JEE(Main+Advanced)",
+                        url: "jee",
+                        submenu: [
+                            {
+                                title: "Class 11th",
+                                url: "online-coaching-class-11",
+                            },
+                            {
+                                title: "Class 12th",
+                                url: "online-coaching-class-12",
+                            },
+                            {
+                                title: "Class 12th Plus",
+                                url: "online-coaching-class-12-droppers",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Study Material",
+        url: "gre",
+    },
+    {
+        title: "Talk to us",
+        url: "schedule-a-call-back",
+    },
+];
