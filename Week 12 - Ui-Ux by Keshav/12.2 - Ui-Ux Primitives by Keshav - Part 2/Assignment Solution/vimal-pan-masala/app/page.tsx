4f78fe1e742bd7d34bd9502f4cb39d736794ef03
Bharat Kumar <bk9955204456@gmail.com>
2024-11-02 17:06:29 +0530

    week 12.2 vimal website assignment solution code added

---
+++ Week 12 - Ui-Ux by Keshav/12.2 - Ui-Ux Primitives by Keshav - Part 2/Assignment Solution/vimal-pan-masala/app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Content from "@/components/Content";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Content />
+      <Footer />
+    </main>
+  );
+}
