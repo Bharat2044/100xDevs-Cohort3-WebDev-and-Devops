353b233940aaa91042bc92a674cc9d3f3ceb25bb
Bharat Kumar <bk9955204456@gmail.com>
2024-11-03 18:35:34 +0530

    13.1 - Tailwind class/lecture code added

---
+++ Week 13/13.1 - Tailwind/Lecture Codes/01_tailwind/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          500: "blue"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
