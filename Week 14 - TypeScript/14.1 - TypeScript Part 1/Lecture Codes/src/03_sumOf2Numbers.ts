141167b712ccca28d9e7166d0f665182edd047fc
Bharat Kumar <bk9955204456@gmail.com>
2024-11-09 21:54:14 +0530

    14.1 - TypeScript Part 1 class/lecture code added

---
+++ Week 14 - TypeScript/14.1 - TypeScript Part 1/Lecture Codes/src/03_sumOf2Numbers.ts
@@ -0,0 +1,12 @@
+// Function that takes 2 numbers and returns the sum of them 
+function sum(a: number, b: number): number {
+
+    // return the sum of the 2 numbers
+    return a + b;
+}
+
+// Call the function with 2 arguments and store the result in a variable called ans
+let ans: number = sum(10, 20);
+
+// Log the result to the console 
+console.log(ans); // 30
