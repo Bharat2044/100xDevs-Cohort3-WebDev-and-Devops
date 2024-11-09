7c13b99e0f2d1e222f6638b612b3b22fef24a8d9
Bharat Kumar <bk9955204456@gmail.com>
2024-11-09 21:53:39 +0530

    14.1 - TypeScript Part 1 class/lecture code added

---
+++ Week 14 - TypeScript/14.1 - TypeScript Part 1/Lecture Codes/src/01_index.ts
@@ -0,0 +1,15 @@
+// Create a variable x and assign a number to it
+let x: number = 10; // type inferencing
+
+// print the value of x to the console
+console.log(x);
+
+// reassign x to a string value 
+// x = "Harkirat"; // error
+
+
+// Create a variable firstName and assign a string to it
+let firstName: string = "Bharat";
+
+// print the value of firstName to the console
+console.log(firstName);
