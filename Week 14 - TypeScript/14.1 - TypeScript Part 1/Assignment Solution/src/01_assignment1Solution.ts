ed4b8fa6919a693ee020b80e09c060bb22197885
Bharat Kumar <bk9955204456@gmail.com>
2024-11-16 18:08:22 +0530

    14.1 - TypeScript Part 1 assignment solution code added

---
+++ Week 14 - TypeScript/14.1 - TypeScript Part 1/Assignment Solution/src/01_assignment1Solution.ts
@@ -0,0 +1,14 @@
+// Assignment #1 - Write a function that calculates the sum of two arguments
+
+
+// Function to calculate the sum of two arguments and return the result
+function sum(a: number, b: number): number {
+    // Return the sum of the two arguments
+    return a + b;
+}
+
+// Call the function and store the result in a variable called ans
+const ans = sum(5, 10);
+
+// Print the result to the console
+console.log(ans); // 15
