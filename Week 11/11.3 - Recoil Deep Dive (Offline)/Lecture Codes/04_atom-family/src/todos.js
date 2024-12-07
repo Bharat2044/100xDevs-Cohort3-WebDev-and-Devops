09d433921658d63766dfd6876b23e1086df2e76f
Bharat Kumar <bk9955204456@gmail.com>
2024-12-07 15:50:46 +0530

    11.3 - Recoil Deep Dive lecture/class code added

---
+++ Week 11/11.3 - Recoil Deep Dive (Offline)/Lecture Codes/04_atom-family/src/todos.js
@@ -0,0 +1,10 @@
+// Create list of todos to be used in the atomFamily and export it
+export const TODOS = [{
+    id: 1,
+    title: "Go to Gym",
+    description: "Hit the gym from 7-9"
+}, {
+    id: 2,
+    title: "Go to eat food",
+    description: "Eat food from from 9-11"
+},]
