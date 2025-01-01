c6ef9705befaa61b0d983a05fa898b1a17b4590e
Bharat Kumar <bk9955204456@gmail.com>
2025-01-01 11:05:25 +0530

    11.3 - Recoil Deep Dive (Offline) class assignment added class/lecture code added

--- Week 11/11.3 - Recoil Deep Dive (Offline)/Lecture Codes/07_use-recoil-state-loadable/src/atoms.js
+++ Week 11/11.3 - Recoil Deep Dive (Offline)/Lecture Codes/07_use-recoil-state-loadable/src/atoms.js
@@ -4,21 +4,23 @@ import { atomFamily, selectorFamily } from "recoil";
 // Import the axios package to make HTTP requests
 import axios from "axios";
 
-// Define an atom family to manage the state for individual todos
+// Define an atom family to manage the state for individual todos fetched from the server and expose it for use in other components
 export const todosAtomFamily = atomFamily({
-    // Define the key for the atom family with a unique identifier 
-    key: 'todosAtomFamily',
-    
-    // Define the default value for the atom family
-    default: selectorFamily({
-        key: "todoSelectorFamily", // Unique key for the selector family
+    key: 'todosAtomFamily', // Unique ID (with respect to other atoms/selectors)
 
-        // Define the get method to fetch the todo data from the server using the given ID
+    // Define a function that returns the initial value for the atom 
+    default: selectorFamily({
+        key: "todoSelectorFamily", // Unique ID (with respect to other atoms/selectors)
+
+        // Define a function that returns the initial value for the atom 
         get: (id) => async ({ get }) => {
-            // Make an HTTP GET request to fetch the todo with the given ID
+            // Wait for 5 seconds before fetching the todo from the server
+            await new Promise(r => setTimeout(r, 5000));
+
+            // Fetch the todo with the given id from the server using the axios 
             const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
 
-            // Return the todo data from the response
+            // Return the todo fetched from the server
             return res.data.todo;
         },
     })
