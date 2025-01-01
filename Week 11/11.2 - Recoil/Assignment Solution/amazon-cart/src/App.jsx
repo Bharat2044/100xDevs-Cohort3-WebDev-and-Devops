aff6e6d8b98ba892fb00a23aa4e3e0eaa205e817
Bharat Kumar <bk9955204456@gmail.com>
2025-01-01 12:00:17 +0530

    11.2 - Recoil assignment amazon-cart solution code added

---
+++ Week 11/11.2 - Recoil/Assignment Solution/amazon-cart/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import { AmazonStyleCart, WishList } from "./components";
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<WishList />} />
+            <Route path="/cart" element={<AmazonStyleCart />} />
+        </Routes>
+    );
+};
+
+export default App;
