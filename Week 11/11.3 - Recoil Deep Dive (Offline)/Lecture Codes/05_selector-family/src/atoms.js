bba22a4fce37e3ef760e52dc0189ab8677d9f128
Bharat Kumar <bk9955204456@gmail.com>
2025-01-01 09:18:55 +0530

    11.3 - Recoil Deep Dive class/lecture code added

--- Week 11/11.3 - Recoil Deep Dive (Offline)/Lecture Codes/05_selector-family/src/atoms.js
+++ Week 11/11.3 - Recoil Deep Dive (Offline)/Lecture Codes/05_selector-family/src/atoms.js
@@ -1,16 +1,60 @@
-// import atomFamily function from recoil library
-import { atomFamily } from "recoil";
+// Import the atomFamily and selectorFamily functions from the recoil package
+import { atomFamily, selectorFamily } from "recoil";
 
-// import TODOS from todos.js file to use in atomFamily
-import { TODOS } from "./todos";
+// Import the axios package to make HTTP requests
+import axios from "axios";
 
-// Create atomFamily with key todosAtomFamily and default value as id 
+// Define an atom family to manage the state for individual todos
 export const todosAtomFamily = atomFamily({
-    // key is the unique identifier for the atomFamily
+    // Define the key for the atom family with a unique identifier 
     key: 'todosAtomFamily',
-    // default is a function that returns the default value of the atomFamily with id as parameter
-    default: id => {
-        // return the todo object with id as parameter from TODOS array 
-        return TODOS.find(x => x.id === id);
-    },
+    // Define the default value for the atom family
+    default: selectorFamily({
+        key: "todoSelectorFamily", // Unique key for the selector family
+
+        // Define the get method to fetch the todo data from the server using the given ID
+        get: (id) => async ({ get }) => {
+            // Make an HTTP GET request to fetch the todo with the given ID
+            const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+
+            // Return the todo data from the response
+            return res.data.todo;
+        },
+    })
 });
+
+/*
+// Define an atom family to manage the state for individual todos
+export const todosAtomFamily = atomFamily({
+    // Define the key for the atom family with a unique identifier 
+    key: 'todosAtomFamily',
+    // Define the default value for the atom family
+    default: selectorFamily({
+        key: "todoSelectorFamily", // Unique key for the selector family
+
+        // Define the get method to fetch the todo data from the server using the given ID
+        get: function (id) {
+            // Return an async function to fetch the todo data from the server
+            return async function ({ get }) {
+                // Make an HTTP GET request to fetch the todo with the given ID
+                const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+
+                // Return the todo data from the response
+                return res.data.todo;
+            }
+        },
+    })
+});
+*/
+
+
+// const todo = atom({
+//     key: 'todo',
+//     default: selector({
+//         key: 'todoSelector',
+//         get: async ({ get }) => {
+//             const res = await axios.get('https://sum-server.100xdevs.com/todo');
+//             return res.data.todo;
+//         }
+//     })
+// })
